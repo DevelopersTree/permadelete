edf6f4df56250ceba72ca715c5f0189be3da4d84
Encryptor <encryptor@outlook.com>
2016-11-09 23:48:48 +0300

    Basic structure and a ver simple mvvm framework

--- RudeFox.FrontEnd/Views/MainWindow.xaml.cs
+++ RudeFox.FrontEnd/Views/MainWindow.xaml.cs
@@ -10,10 +10,9 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace RudeFox.FrontEnd
+namespace RudeFox.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
