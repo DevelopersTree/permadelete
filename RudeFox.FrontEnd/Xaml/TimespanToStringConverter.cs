2f4192e68c9b1e61786db0b3b0ab87fef96a6fda
Encryptor <encryptor@outlook.com>
2016-12-28 02:35:50 +0300

    Rename RudeFox.Converters to RudeFox.Xaml

--- RudeFox.FrontEnd/Xaml/TimespanToStringConverter.cs
+++ RudeFox.FrontEnd/Xaml/TimespanToStringConverter.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Data;
 
-namespace RudeFox.Converters
+namespace RudeFox.Xaml
 {
     class TimespanToStringConverter : IValueConverter
     {
