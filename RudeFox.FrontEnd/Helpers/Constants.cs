265f70c3c18709ee871d25322ca4e3854a93f108
Encryptor <encryptor@outlook.com>
2017-01-02 18:04:16 +0300

    Removed sendto from the constants

--- RudeFox.FrontEnd/Helpers/Constants.cs
+++ RudeFox.FrontEnd/Helpers/Constants.cs
@@ -13,7 +13,5 @@ namespace RudeFox.Helpers
         public const int GIGABYTE = MEGABYTE * KILOBYTE;
 
         public const string WEBSITE_URL = "https://github.com/encrypt0r/RudeFox";
-
-        public const string SENDTO_PREFIX = "--sendto";
     }
 }
